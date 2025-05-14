ec2c1e2bd732aabe15e80c4c7f3064d7328c7cd0
Nisarg Shah <nisarg.shah@veeva.com>
2025-05-14 11:00:40 -0700

    remove unnecessary files

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,17 +6,8 @@ export const setupFiles = [path.join(import.meta.dirname, "./vitestSetup.js")];
 
 export default defineConfig({
   plugins: [react()],
-
   test: {
     coverage: {
-      /**
-       * Only add exclude patterns for one of the following reasons:
-       *  - It's a general exclude glob
-       *  - The entire workspace package should be excluded (mocks, test code, etc)
-       *
-       * Do not add direct files here to ignore
-       */
-
       reporter: ["text"],
       skipFull: true,
       thresholds: {
